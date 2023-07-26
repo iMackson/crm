ac26e075d0b69df02a5411de143813539005c793
Mackson Isani <isanimackson@gmail.com>
2023-07-26 16:25:19 +0100

    Changed leads url

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Lead
+
 
 def home_page(request):
-    # return HttpResponse("Hello World")
-    return render(request, "leads/home_page.html")
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, "leads/home_page.html", context)
