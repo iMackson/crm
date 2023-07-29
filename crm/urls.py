b256336073f319836ce0e4f5a6b9776247ff1421
Mackson Isani <isanimackson@gmail.com>
2023-07-29 09:34:40 +0100

    Added forms and views

--- crm/urls.py
+++ crm/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('leads/', include('leads.urls'), name='leads'),
+    path('leads/', include('leads.urls', namespace='leads')),
 ]
--- leads/urls.py
+++ leads/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = "leads"
 
 urlpatterns = [
-    path('', views.home_page, name='homepage')
+    path('', views.lead_list, name='lead_list'),
+    path('<int:pk>/', views.lead_detail, name='detail'),
+    path('<int:pk>/update/', views.lead_update, name='update'),
+    path('create/', views.lead_create, name="create"),
+    path('<int:pk>/delete/', views.lead_delete, name="delete")
 ]
